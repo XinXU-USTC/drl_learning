dee0a8305fca990e756b29df0f8f89b4c8ff5f73
XinXU <xxuca@connec.ust.hk>
2022-08-27 02:05:02 +0800

    QLearning

---
+++ algorithms/QLearning/main.py
@@ -0,0 +1,158 @@
+import turtle
+import gym
+import numpy as np
+import logging
+from tqdm import tqdm
+import os
+
+from algorithms.QLearning.agent import Agent
+
+
+class CliffWalkingWapper(gym.Wrapper):
+    def __init__(self, env):
+        gym.Wrapper.__init__(self, env)
+        self.t = None
+        self.unit = 50
+        self.max_x = 12
+        self.max_y = 4
+
+    def draw_x_line(self, y, x0, x1, color='gray'):
+        assert x1 > x0
+        self.t.color(color)
+        self.t.setheading(0)
+        self.t.up()
+        self.t.goto(x0, y)
+        self.t.down()
+        self.t.forward(x1 - x0)
+
+    def draw_y_line(self, x, y0, y1, color='gray'):
+        assert y1 > y0
+        self.t.color(color)
+        self.t.setheading(90)
+        self.t.up()
+        self.t.goto(x, y0)
+        self.t.down()
+        self.t.forward(y1 - y0)
+
+    def draw_box(self, x, y, fillcolor='', line_color='gray'):
+        self.t.up()
+        self.t.goto(x * self.unit, y * self.unit)
+        self.t.color(line_color)
+        self.t.fillcolor(fillcolor)
+        self.t.setheading(90)
+        self.t.down()
+        self.t.begin_fill()
+        for i in range(4):
+            self.t.forward(self.unit)
+            self.t.right(90)
+        self.t.end_fill()
+
+    def move_player(self, x, y):
+        self.t.up()
+        self.t.setheading(90)
+        self.t.fillcolor('red')
+        self.t.goto((x + 0.5) * self.unit, (y + 0.5) * self.unit)
+
+    def render(self):
+        if self.t == None:
+            self.t = turtle.Turtle()
+            self.wn = turtle.Screen()
+            self.wn.setup(self.unit * self.max_x + 100,
+                          self.unit * self.max_y + 100)
+            self.wn.setworldcoordinates(0, 0, self.unit * self.max_x,
+                                        self.unit * self.max_y)
+            self.t.shape('circle')
+            self.t.width(2)
+            self.t.speed(0)
+            self.t.color('gray')
+            for _ in range(2):
+                self.t.forward(self.max_x * self.unit)
+                self.t.left(90)
+                self.t.forward(self.max_y * self.unit)
+                self.t.left(90)
+            for i in range(1, self.max_y):
+                self.draw_x_line(
+                    y=i * self.unit, x0=0, x1=self.max_x * self.unit)
+            for i in range(1, self.max_x):
+                self.draw_y_line(
+                    x=i * self.unit, y0=0, y1=self.max_y * self.unit)
+
+            for i in range(1, self.max_x - 1):
+                self.draw_box(i, 0, 'black')
+            self.draw_box(self.max_x - 1, 0, 'yellow')
+            self.t.shape('turtle')
+
+        x_pos = self.s % self.max_x
+        y_pos = self.max_y - 1 - int(self.s / self.max_x)
+        self.move_player(x_pos, y_pos)
+
+class Runner():
+    def __init__(self, args, config):
+        self.env = self.set_env(config.env)
+        self.args = args
+        self.config = config
+        self.n_states = self.env.observation_space.n
+        self.n_actions = self.env.action_space.n
+        config.n_actions = self.n_actions
+        config.n_states = self.n_states
+        self.agent = Agent(args, config)
+
+    def set_env(self, env_name):
+        if env_name == 'CliffWalking-v0':
+            env = gym.make(env_name)
+            env = CliffWalkingWapper(env)
+        return env
+
+    def train(self):
+        args, config = self.args, self.config
+        agent, env = self.agent, self.env
+        tb_logger = config.tb_logger
+        logging.info(f"Start Training! Env: {config.env}, Algorithm: {args.alg}")
+        for episode in tqdm(range(config.train.n_episode), desc = 'Episodes'):
+            ep_reward, ep_step = 0, 0
+            state = env.reset()
+            while True:
+                action = agent.sample_action(state, episode)
+                next_state, reward, done, _ = env.step(action)
+                agent.update(state, action, reward, next_state, done)
+                state = next_state
+                ep_reward += reward
+                ep_step += 1
+                #logging.info(
+                #    f"episode: {episode}, step: {ep_step}, reward: {reward}, accum_reward: {ep_reward}"
+                #)
+                if done:
+                    break
+            tb_logger.add_scalar("reward", ep_reward, global_step=episode)
+            logging.info(
+                f"episode: {episode}, steps: {ep_step}, reward: {ep_reward}"
+            )
+        logging.info(f"End Training! {config.train.n_episode} Episodes!")
+        agent.save_model(args.log_path) 
+
+    def test(self):
+        args, config = self.args, self.config
+        agent, env = self.agent, self.env
+        tb_logger = config.tb_logger 
+        logging.info(f"Start Testing! Env: {config.env}, Algorithm: {args.alg}")
+        agent.load_model(args.log_path)
+        for episode in tqdm(range(config.test.n_episode), desc = 'Episodes'):
+            ep_reward, ep_step = 0, 0
+            state = env.reset()
+            while True:
+                action = agent.predict_action(state)
+                state, reward, done, _ = env.step(action)
+                ep_reward += reward
+                ep_step += 1
+                #logging.info(
+                #    f"episode: {episode}, step: {ep_step}, reward: {reward}, accum_reward: {ep_reward}"
+                #)
+                if done:
+                    break
+            tb_logger.add_scalar("reward", ep_reward, global_step=episode)
+            logging.info(
+                f"episode: {episode}, steps: {ep_step}, reward: {ep_reward}"
+            ) 
+        logging.info(f"End Training! {config.test.n_episode} Episodes!") 
+
+
---
+++ main.py
@@ -0,0 +1,145 @@
+import argparse
+import yaml
+import sys
+import os 
+import torch
+import numpy as np
+import torch.utils.tensorboard as tb
+import logging
+import shutil
+import traceback
+import importlib
+
+
+torch.set_printoptions(sci_mode = False)
+
+
+def parse_args_and_config():
+    parser = argparse.ArgumentParser(description=globals()["__doc__"])
+
+    parser.add_argument(
+        "--alg", 
+        type=str, 
+        required=True, 
+        help="Choose an algorithm to run"
+    )
+    
+    parser.add_argument(
+        "--train", 
+        action="store_true", 
+        help="whether to train the model"
+    )
+
+    parser.add_argument(
+        "--test",
+        action="store_true",
+        help="Whether to test the model"
+    )
+    
+    parser.add_argument(
+        "--verbose",
+        type=str,
+        default="info",
+        help="Verbose level: info | debug | warning | critical"
+    )
+
+    parser.add_argument(
+        "--comment",
+        type=str,
+        default="",
+        help="A string for experiment comment"
+    )
+
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=123,
+        help="Random seed"
+    )
+
+    args = parser.parse_args()
+    args.alg_path = os.path.join("algorithms", args.alg)
+    args.log_path = os.path.join(args.alg_path, "logs")
+    tb_path = os.path.join(args.alg_path, "tensorboard")
+
+    # parse config file
+    with open(os.path.join(args.alg_path, "config.yml"), "r") as f:
+        config = yaml.safe_load(f)
+    new_config = dict2namespace(config)
+
+    level = getattr(logging, args.verbose.upper(), None)
+    if not isinstance(level, int):
+        raise ValueError("level {} not supported".format(args.verbose))
+
+    handler1 = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "%(levelname)s - %(filename)s - %(asctime)s - %(message)s"
+    )
+    handler1.setFormatter(formatter)
+
+    if args.train:
+        if os.path.exists(args.log_path):
+            shutil.rmtree(args.log_path)
+            shutil.rmtree(tb_path)
+        os.makedirs(args.log_path)
+        with open(os.path.join(args.log_path, "config.yml"), "w") as f:
+            yaml.dump(new_config, f, default_flow_style=False)
+        handler2 = logging.FileHandler(os.path.join(args.log_path, "stdout.txt"))
+        handler2.setFormatter(formatter)
+    new_config.tb_logger = tb.SummaryWriter(log_dir=tb_path)
+    logger = logging.getLogger()
+    logger.addHandler(handler1)
+    if args.train:
+        logger.addHandler(handler2)
+    logger.setLevel(level)
+
+    # add device
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    logging.info("Using device:{}".format(device))
+    new_config.device = device
+
+    # set random seed
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(args.seed)
+
+    torch.backends.cudnn.benchmark = True
+
+    return args, new_config
+
+
+
+def dict2namespace(config):
+    namespace = argparse.Namespace()
+    for key, value in config.items():
+        if isinstance(value, dict):
+            new_value = dict2namespace(value)
+        else:
+            new_value = value
+        setattr(namespace, key, new_value) 
+    return namespace
+
+def main():
+    args, config = parse_args_and_config()
+    logging.info("Writing log file to {}".format(args.log_path))
+    logging.info("Exp instance id = {}".format(os.getpid()))
+    logging.info("Exp comment = {}".format(args.comment))
+    
+    #params_agent = importlib.import_module(args.alg_path+".agent")
+    #agent = params_agent.Agent()
+    params = importlib.import_module("algorithms."+args.alg +".main")
+    runner = params.Runner(args, config)
+    try:
+        if args.train:
+            runner.train()
+        elif args.test:
+            runner.test()
+    except Exception:
+        logging.info(traceback.format_exc())
+    
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
